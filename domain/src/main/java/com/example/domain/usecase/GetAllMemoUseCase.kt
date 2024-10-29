eb87ee365a031940b48517c0f0a02414d9c429c1
kimjunsu <12jangdokg55@gmail.com>
2024-10-29 21:09:33 +0900

    [refactor] getAllMemo method is Flow<List<MemoEntity>> to List<MemoEntity>

--- domain/src/main/java/com/example/domain/usecase/GetAllMemoUseCase.kt
+++ domain/src/main/java/com/example/domain/usecase/GetAllMemoUseCase.kt
@@ -2,11 +2,10 @@ package com.example.domain.usecase
 
 import com.example.domain.model.MemoModel
 import com.example.domain.repository.MemoRepository
-import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetAllMemoUseCase @Inject constructor(
     private val memoRepository: MemoRepository
 ) {
-    operator fun invoke(): Flow<List<MemoModel>> = memoRepository.getAllMemo()
+    suspend operator fun invoke(): List<MemoModel> = memoRepository.getAllMemo()
 }
